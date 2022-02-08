bdf90318fccb4481c69d4af809bc46ab604f1247
Awen <2889984509@qq.com>
2022-02-08 20:43:10 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./app.scss"
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <div className="box"></div>
+    </div>
+  );
 }
