6016cad683b6f1c68bc3857ca4014d10897a9f65
Awen <2889984509@qq.com>
2022-02-08 21:53:55 +0800

    init router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import "./app.scss"
+import React, { Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+const Home = React.lazy(() => import("./pages/Home"));
+const Login = React.lazy(() => import("./pages/Login"));
 
 export default function App() {
   return (
-    <div>
-      App
-      <div className="box"></div>
-    </div>
+    <Router>
+      <div className="app">
+        <Suspense fallback={<div>loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
